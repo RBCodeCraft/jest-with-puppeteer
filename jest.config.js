e3204b6bf83a352cc609ee488f26aa88cf28c2dd
Russell Briggs <russ@paraflyer.net>
2018-04-28 23:14:22 +1200

    Added jest-puppeteer and sample test

--- jest.config.js
+++ jest.config.js
@@ -14,4 +14,5 @@ module.exports = {
         "json",
         "node"
     ],
+    "preset": "jest-puppeteer"
 }
--- tests/google.tests.ts
+++ tests/google.tests.ts
@@ -1,8 +1,19 @@
 
-describe('google', () => {
+jest.setTimeout(10000);
 
-    it('works', () => {
-        expect(true).toBe(true);
+describe('Google', () => {
+
+    beforeAll(async () => {
+        await page.goto('https://google.com');
+    });
+
+    it('should display "google" text on page', async () => {
+        await expect(page).toMatch('google');
+    });
+
+    it('can enter text in search box', async () => {
+        await expect(page).toFill('input[name="q"]', 'RevJS');
+        await new Promise((resolve) => setTimeout(resolve, 5000));
     });
 
 });
